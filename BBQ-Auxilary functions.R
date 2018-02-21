ff0f07cf7dd997b20a6b7007678984745425ac1c
MichaelGurkov <Michael.gurkov@mail.huji.ac.il>
2018-02-21 10:32:50 +0200

    Added peaks selecting functions: get.max.peak get.alternating.peaks
    needs to be optimized

--- BBQ-Auxilary functions.R
+++ BBQ-Auxilary functions.R
@@ -32,3 +32,40 @@ identify.turning.point = function(window){
   
 }
 
+get.max.peak = function(peaks,start_period,end_period){
+  
+  # browser()
+  
+  sub_peaks = peaks[index(peaks) >= start_period & index(peaks) <= end_period,]
+  
+  if (length(sub_peaks) > 0) {
+    
+    return(sub_peaks[sub_peaks[,1] == max(sub_peaks[,1]) ,])
+    
+  }
+}
+
+get.alternating.peaks = function(peaks, troughs, timeframe){
+  
+  start_points = index(troughs)
+  
+  end_points = c(index(troughs)[-1],timeframe[length(timeframe)])
+  
+  points = cbind.data.frame(start_points,
+                       end_points)
+  
+  names(points) = c("Start_Point","End_Point")
+  
+  points = apply(points, 1, list)
+  
+  alt.peaks = sapply(points,
+         function(Z,peaks){return(get.max.peak(start_period = Z[[1]][1],
+                                         end_period = Z[[1]][2],
+                                         peaks = peaks))},
+         peaks = peaks)
+  
+  alt.peaks = do.call(rbind.xts,alt.peaks)
+  
+}
+
+
